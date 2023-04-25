0323dd1b3260307cb7f5b6192f40b89550896756
Ameziane H <ameziane.hamlat@consensys.net>
2023-04-25 16:17:17 +0200

    Add database path as an argument

--- src/main/java/org/ahamlat/App.java
+++ src/main/java/org/ahamlat/App.java
@@ -20,54 +20,64 @@ import java.util.List;
  */
 public class App 
 {
-    private static final String PATH = "/data/besu/database";
     public static void main(String[] args) throws RocksDBException {
-        RocksDB.loadLibrary();
-        Options options = new Options();
-        options.setCreateIfMissing(true);
-
-        // Open the RocksDB database with multiple column families
-        List<byte[]> cfNames = RocksDB.listColumnFamilies(options, PATH );
-        List<ColumnFamilyHandle> cfHandles = new ArrayList<>();
-        List<ColumnFamilyDescriptor> cfDescriptors = new ArrayList<>();
-        for (byte[] cfName : cfNames) {
-            cfDescriptors.add(new ColumnFamilyDescriptor(cfName));
+        String dbPath = null;
+        for (String arg : args) {
+            if (arg.startsWith("--dbPath=")) {
+                dbPath = arg.substring(9);
+            }
         }
-        boolean emptyColumnFamily ;
-        try (final RocksDB rocksdb = RocksDB.openReadOnly (PATH, cfDescriptors,cfHandles)) {
-            for (int i = 0; i < cfNames.size(); i++) {
-                emptyColumnFamily = false;
-                byte[] cfName = cfNames.get(i);
-                ColumnFamilyHandle cfHandle = cfHandles.get(i);
-                String size = rocksdb.getProperty(cfHandle, "rocksdb.estimate-live-data-size");
-                if (!size.isEmpty()) {
-                    long sizeLong = Long.parseLong(size);
-                    if (sizeLong == 0) emptyColumnFamily = true;
-                    if (!emptyColumnFamily) System.out.println("****** Column family '" + getNameById(cfName) + "' size: " +formatOutputSize(sizeLong)+ " ******");
+        if (dbPath != null) {
+            RocksDB.loadLibrary();
+            Options options = new Options();
+            options.setCreateIfMissing(true);
+
+            // Open the RocksDB database with multiple column families
+            List<byte[]> cfNames = RocksDB.listColumnFamilies(options, dbPath);
+            List<ColumnFamilyHandle> cfHandles = new ArrayList<>();
+            List<ColumnFamilyDescriptor> cfDescriptors = new ArrayList<>();
+            for (byte[] cfName : cfNames) {
+                cfDescriptors.add(new ColumnFamilyDescriptor(cfName));
+            }
+            boolean emptyColumnFamily;
+            try (final RocksDB rocksdb = RocksDB.openReadOnly(dbPath, cfDescriptors, cfHandles)) {
+                for (int i = 0; i < cfNames.size(); i++) {
+                    emptyColumnFamily = false;
+                    byte[] cfName = cfNames.get(i);
+                    ColumnFamilyHandle cfHandle = cfHandles.get(i);
+                    String size = rocksdb.getProperty(cfHandle, "rocksdb.estimate-live-data-size");
+                    if (!size.isEmpty()) {
+                        long sizeLong = Long.parseLong(size);
+                        if (sizeLong == 0) emptyColumnFamily = true;
+                        if (!emptyColumnFamily)
+                            System.out.println("****** Column family '" + getNameById(cfName) + "' size: " + formatOutputSize(sizeLong) + " ******");
+                    }
+                    // System.out.println("SST table : "+ rocksdb.getProperty(cfHandle, "rocksdb.sstables"));
+                    if (!emptyColumnFamily) {
+                        System.out.println("Number of live snapshots : " + rocksdb.getProperty(cfHandle, "rocksdb.num-snapshots"));
+                        String totolSstFilesSize = rocksdb.getProperty(cfHandle, "rocksdb.total-sst-files-size");
+                        if (!totolSstFilesSize.isEmpty() && !totolSstFilesSize.isBlank()) {
+                            System.out.println("Total size of SST Files : " + formatOutputSize(Long.parseLong(totolSstFilesSize)));
+                        }
+                        String liveSstFilesSize = rocksdb.getProperty(cfHandle, "rocksdb.live-sst-files-size");
+                        if (!liveSstFilesSize.isEmpty() && !liveSstFilesSize.isBlank()) {
+                            System.out.println("Size of live SST Filess : " + formatOutputSize(Long.parseLong(liveSstFilesSize)));
+                        }
+
+                        ColumnFamilyMetaData columnFamilyMetaData = rocksdb.getColumnFamilyMetaData(cfHandle);
+                        long sizeBytes = columnFamilyMetaData.size();
+                        System.out.println("Column family size (with getColumnFamilyMetaData) : " + formatOutputSize(sizeBytes));
+                        System.out.println("");
+
+                    }
                 }
-                // System.out.println("SST table : "+ rocksdb.getProperty(cfHandle, "rocksdb.sstables"));
-                if (!emptyColumnFamily) {
-                    System.out.println("Number of live snapshots : " + rocksdb.getProperty(cfHandle, "rocksdb.num-snapshots"));
-                    String totolSstFilesSize = rocksdb.getProperty(cfHandle, "rocksdb.total-sst-files-size");
-                    if (!totolSstFilesSize.isEmpty() && !totolSstFilesSize.isBlank()) {
-                        System.out.println("Total size of SST Files : " + formatOutputSize(Long.parseLong(totolSstFilesSize)));
-                    }
-                    String liveSstFilesSize = rocksdb.getProperty(cfHandle, "rocksdb.live-sst-files-size");
-                    if (!liveSstFilesSize.isEmpty() && !liveSstFilesSize.isBlank()) {
-                        System.out.println("Size of live SST Filess : " + formatOutputSize(Long.parseLong(liveSstFilesSize)));
-                    }
-
-                    ColumnFamilyMetaData columnFamilyMetaData = rocksdb.getColumnFamilyMetaData(cfHandle);
-                    long sizeBytes = columnFamilyMetaData.size();
-                    System.out.println("Column family size (with getColumnFamilyMetaData) : "+formatOutputSize(sizeBytes));
-                    System.out.println("");
-
+            } finally {
+                for (ColumnFamilyHandle cfHandle : cfHandles) {
+                    cfHandle.close();
                 }
             }
-        } finally {
-            for (ColumnFamilyHandle cfHandle : cfHandles) {
-                cfHandle.close();
-            }
+        } else {
+            System.out.println("Database path not provided");
         }
     }
 
